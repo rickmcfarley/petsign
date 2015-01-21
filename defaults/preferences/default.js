79facd83de786996fcacc097bb1f3217cada7f8d
Rick McFarley <rickmcfarley@gmail.com>
2015-01-21 18:54:02 -0500

    Delete default.js

--- defaults/preferences/default.js
+++
@@ -1,9 +0,0 @@
-pref("extensions.petsign.fname", "First Name");
-pref("extensions.petsign.lname", "Last Name");
-pref("extensions.petsign.email", "Email");
-pref("extensions.petsign.address", "Address");
-pref("extensions.petsign.postalcode", "Zip Code");
-pref("extensions.petsign.message", "");
-pref("extensions.petsign.shareinfo", "False");
-pref("extensions.petsign.public", "False");
-pref("extensions.petsign.prompt", "True");
